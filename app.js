b5fba3cf9dfe8bbdb62a108b9a600e0fffd59c50
aapwoka <65490601+aapwoka@users.noreply.github.com>
2023-01-07 22:24:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -94,15 +94,15 @@ function checkUserBio(){
 function signUp(){
     var userFullName = document.getElementById("userFullName").value;
     var userSurname = document.getElementById("userSurname").value;
-    var userEmail = document.getElementById("userEmail").value;
-    var userPassword = document.getElementById("userPassword").value;
+    var email = document.getElementById("userEmail").value;
+    var password = document.getElementById("userPassword").value;
     var userFullNameFormate = /^([A-Za-z.\s_-])/;    
     var userEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     var userPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;      
 
     var checkUserFullNameValid = userFullName.match(userFullNameFormate);
-    var checkUserEmailValid = userEmail.match(userEmailFormate);
-    var checkUserPasswordValid = userPassword.match(userPasswordFormate);
+    var checkUserEmailValid = email.match(userEmailFormate);
+    var checkUserPasswordValid = password.match(userPasswordFormate);
 
     if(checkUserFullNameValid == null){
         return checkUserFullName();
